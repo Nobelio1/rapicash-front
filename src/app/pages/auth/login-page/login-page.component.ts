a459b4dc5206ab8472c3be0e62b65e73f3b643de
Gino Alejandro <ginoalejandro2304@gmail.com>
2024-11-30 01:30:47 -0500

    cambio en redireccion de rutas

--- src/app/pages/auth/login-page/login-page.component.ts
+++ src/app/pages/auth/login-page/login-page.component.ts
@@ -6,42 +6,58 @@ import {AuthService} from "../../../services/auth.service";
 import {SesionRequestI} from "../../../interfaces/auth.interface";
 
 @Component({
-  selector: 'app-login-page',
-  standalone: true,
-  imports: [RouterLink, ReactiveFormsModule, CommonModule],
-  templateUrl: './login-page.component.html',
-  styles: ``
+    selector: 'app-login-page',
+    standalone: true,
+    imports: [RouterLink, ReactiveFormsModule, CommonModule],
+    templateUrl: './login-page.component.html',
+    styles: ``
 })
-export class LoginPageComponent implements OnInit{
+export class LoginPageComponent implements OnInit {
 
 
-  private form = inject(FormBuilder);
-  private authService = inject(AuthService);
-  private router = inject(Router);
+    private form = inject(FormBuilder);
+    private authService = inject(AuthService);
+    private router = inject(Router);
 
-  ngOnInit(): void {
-    this.clean()
-  }
+    ngOnInit(): void {
+        this.clean()
+    }
 
-  public myForm = this.form.group({
-    correo:    ['', [ Validators.required, Validators.email ]],
-    contrasena: ['', [ Validators.required, Validators.minLength(3) ]],
-  });
+    public myForm = this.form.group({
+        correo: ['', [Validators.required, Validators.email]],
+        contrasena: ['', [Validators.required, Validators.minLength(3)]],
+    });
 
-  login() {
-    if (this.myForm.invalid) return;
-    const {correo, contrasena} = this.myForm.value;
-    const user: SesionRequestI = {correo: correo!, contrasena: contrasena!};
+    login() {
+        if (this.myForm.invalid) return;
+        const {correo, contrasena} = this.myForm.value;
+        const user: SesionRequestI = {correo: correo!, contrasena: contrasena!};
 
-    this.authService.login(user).subscribe({
-      next: () => this.router.navigateByUrl('/brc/operations'),
-      error: (message) => console.log('Error', message)
-    })
-  }
+        this.authService.login(user).subscribe({
+            next: () => {
+                const {rolId} = JSON.parse(localStorage.getItem('user')!);
+                switch (rolId) {
+                    case 1:
+                        this.router.navigateByUrl('/brc/operations')
+                        break;
+                    case 2:
+                        this.router.navigateByUrl('/brc/financial-requests')
+                        break;
+                    case 3:
+                        this.router.navigateByUrl('/brc/credit-requests')
+                        break;
+                    case 4:
+                        this.router.navigateByUrl('/brc/oficial-requests')
+                        break;
+                }
+            },
+            error: (message) => console.log('Error', message)
+        })
+    }
 
-  clean(){
-    localStorage.clear()
-  }
+    clean() {
+        localStorage.clear()
+    }
 
 
 }
