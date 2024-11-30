bfbdb7ac51358a82a21679ab7de1fba70be7ed3e
Gino Alejandro <ginoalejandro2304@gmail.com>
2024-11-30 01:42:30 -0500

    cambio en boton de documentacion

--- src/app/pages/home/financial-summary/financial-summary.component.ts
+++ src/app/pages/home/financial-summary/financial-summary.component.ts
@@ -3,33 +3,45 @@ import {SummaryComponent} from '../../../components/summary/summary.component';
 import {TitlebarComponent} from '../../../components/titlebar/titlebar.component';
 import {SolicitudService} from "../../../services/solicitud.service";
 import {ActualizarEstado} from "../../../interfaces/solicitud.interface";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {CommonModule} from "@angular/common";
 
 @Component({
-  selector: 'app-financial-summary',
-  standalone: true,
-  imports: [
-    SummaryComponent,
-    TitlebarComponent
-  ],
-  templateUrl: './financial-summary.component.html',
-  styles: ``
+    selector: 'app-financial-summary',
+    standalone: true,
+    imports: [
+        SummaryComponent,
+        TitlebarComponent,
+        CommonModule
+    ],
+    templateUrl: './financial-summary.component.html',
+    styles: ``
 })
 export class FinancialSummaryComponent {
 
-  private solicitudService = inject(SolicitudService);
-  private params = inject(ActivatedRoute)
+    private solicitudService = inject(SolicitudService);
+    private params = inject(ActivatedRoute)
+    private router = inject(Router);
 
-  actualizarEstado(opt: number){
-    const estado : ActualizarEstado = {
-        solicitudId: +this.params.snapshot.paramMap.get('id')!,
-        estadoFinan: ''
+    public documentacion: boolean = false;
+
+    actualizarEstado(opt: number) {
+        const estado: ActualizarEstado = {
+            solicitudId: +this.params.snapshot.paramMap.get('id')!,
+            estadoFinan: ''
+        }
+        opt === 1 ? estado.estadoFinan = 'APROBADO' : estado.estadoFinan = 'RECHAZADO';
+
+        this.solicitudService.actualizarEstadoFinaciamiento(estado).subscribe(res => {
+            this.router.navigate(['brc/financial-requests']);
+        });
     }
-    opt === 1 ? estado.estadoFinan = 'APROBADO' : estado.estadoFinan = 'RECHAZADO';
 
-    this.solicitudService.actualizarEstadoFinaciamiento(estado).subscribe(res => {
-      console.log(res);
-    });
-  }
+    verificarDocumentaion(){
+        this.documentacion = true;
+        setTimeout(() => {
+            this.documentacion = false;
+        }, 3000)
+    }
 
 }
